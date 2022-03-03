00af17ba89397d744173d59642822d18ca375898
kawinthep1 <98872342+kawinthep1@users.noreply.github.com>
2022-03-03 15:11:21 +0700

    Create table.php

---
+++ table.php
@@ -0,0 +1,42 @@
+<html>
+  <head>
+    <title>การสร้างตาราง</title>
+  </head>
+  <body>
+    <table border="1">
+      <tr>
+        <th>ลำดับ</th>
+        <th>ชื่อ</th>
+        <th>รูปภาพ</th>
+        <th>TW/FB</th>
+      </tr>
+      <tr>
+        <td>1</td>
+        <td>Elon Musk</td>
+        <td><img src="https://upload.wikimedia.org/wikipedia/commons/3/34/Elon_Musk_Royal_Society_%28crop2%29.jpg" width="45px"></td>
+        <td><a href="https://twitter.com/elonmusk">TW</a></td>
+         <tr>
+        <td>2</td>
+        <td>Mark zuckerberg</td>
+        <td><img src="https://s.isanook.com/hi/0/rp/r/w728/ya0xa0m1w0/aHR0cHM6Ly9zLmlzYW5vb2suY29tL2hpLzAvdWQvMzA4LzE1NDMyODkvMTU0MzI4OS0yMDIxMTAwNjAzMDA1NS1iOWRhNzAwLmpwZw==.jpg" width="45px"></td>
+        <td><a href="https://www.facebook.com/zuck">FB</a></td>
+            <tr>
+        <td>3</td>
+        <td>Lisa su</td>
+        <td><img src="https://www.overclockzone.com/_admin/covercontent/5450-1626040090.jpg" width="45px"></td>
+        <td><a href="https://twitter.com/LisaSu">Facebook</a></td>
+               <tr>
+        <td>4</td>
+        <td>FS LAMMYSNAX</td>
+        <td><img src="https://fullsense.gg/wp-content/uploads/2022/01/Lammy.png" width="45px"></td>
+        <td><a href="https://www.facebook.com/LammyVAL">Facebook</a></td>
+                  <tr>
+        <td>5</td>
+        <td>FS CHALALALA</td>
+        <td><img src="https://fullsense.gg/wp-content/uploads/2022/01/Chlalala.png" width="45px"></td>
+        <td><a href="https://www.instagram.com/g_chanitpak/">Instagram</a></td>
+                     <tr>
+      </tr>
+    </table>
+  </body>
+</html>
