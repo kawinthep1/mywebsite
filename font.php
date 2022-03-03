e5ccef142cc940f7a83085a26dcb029775f7c119
kawinthep1 <98872342+kawinthep1@users.noreply.github.com>
2022-03-03 15:03:19 +0700

    Create font.php

---
+++ font.php
@@ -0,0 +1,25 @@
+<html>
+  <head>
+    <title>การกำหนดรูปแบบตัวอักษร</title>
+    <style>
+      @font-face{
+        font-family: "MyFont1";
+        src: url("Mali-Light.ttf");
+      }
+      @font-face{
+        font-family: "MyFont2";
+        src: url("ChakraPetch-MediumItalic.ttf");
+      }
+      #font1{
+        font-family: "MyFont1";
+      }
+      #font2{
+        font-family: "MyFont2";
+      }
+    </style>
+  </head>
+  <body>
+    <h1 id="font1">กวินเทพ บุญมาคํา ม.4/12 เลขที่ 2 เลขประจำตัว 57010</h1>
+    <h1 id="font2">กวินเทพ บุญมาคํา ม.4/12 เลขที่ 2 เลขประจำตัว 57010</h1>
+  </body>
+</html>
